0d451fa8a2b873a8eafa84d92649a705d1cf206c
Nikita <belka.nik02@gmail.com>
2025-09-03 12:20:54 +0300

    test

---
+++ src/pages/Index.tsx
@@ -0,0 +1,27 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import ServicesSection from "@/components/ServicesSection";
+import DiagnosticsSection from "@/components/DiagnosticsSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import BookingSection from "@/components/BookingSection";
+import ContactsSection from "@/components/ContactsSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen pt-16">
+        <HeroSection />
+        <ServicesSection />
+        <DiagnosticsSection />
+        <TestimonialsSection />
+        <BookingSection />
+        <ContactsSection />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Index;
